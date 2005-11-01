cc9e377f937312aa8b9f200b16fd203412da0ebc
Ralf Schlatterbeck <rsc@runtux.com>
2005-11-01 15:16:12 +0000

    Added license

--- pgamodule.c
+++ pgamodule.c
@@ -1,3 +1,25 @@
+/* Copyright (C) 2005 Dr. Ralf Schlatterbeck Open Source Consulting.
+ * Reichergasse 131, A-3411 Weidling.
+ * Web: http://www.runtux.com Email: office@runtux.com
+ * All rights reserved
+ * ****************************************************************************
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * ****************************************************************************
+ */
+
 #include <Python.h>
 #include <pgapack.h>
 #include <stdio.h>
