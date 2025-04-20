a3f071704eb611d59088b755f443397d19bb07ff
Ralf Schlatterbeck <rsc@runtux.com>
2025-04-20 10:25:28 +0200

    Fix license identifier

--- pga/testsupport.py
+++ pga/testsupport.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Copyright (C) 2022 Dr. Ralf Schlatterbeck Open Source Consulting.
+# Copyright (C) 2022-24 Dr. Ralf Schlatterbeck Open Source Consulting.
 # Reichergasse 131, A-3411 Weidling.
 # Web: http://www.runtux.com Email: office@runtux.com
 # ****************************************************************************
